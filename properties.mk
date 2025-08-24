5b62c93a3ebea1331817bf59176b36876a055df2
FrostLab81 <thiagodd334@gmail.com>
2025-08-24 18:40:27 -0300

    sm6150-common: remove FRP

--- properties.mk
+++ properties.mk
@@ -156,10 +156,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.fm.use_audio_session=true
 
-# FRP
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.frp.pst=/dev/block/bootdevice/by-name/frp
-
 # GPS
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.overlay.izat.optin=rro
