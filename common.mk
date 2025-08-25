e3c71f313e25c9434e0472b46031dca8f3eee8d2
FrostLab81 <thiagodd334@gmail.com>
2025-08-25 10:53:39 -0300

    sm6150-common: bringup bluetooth

--- common.mk
+++ common.mk
@@ -182,7 +182,8 @@ PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    android.hardware.bluetooth@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
